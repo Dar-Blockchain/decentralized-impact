06d18d4c56855fdfc91aa534ae41e7de2357c264
khaldi505 <ihebkhaldi505@gmail.com>
2022-08-31 09:14:44 +0100

    feature: sign up request test

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 const SignUp = () => {
     const signUptesting = () => {
-
+      axios.post('http://localhost:5000/')
     }
   return (
     <div>SignUp</div>
