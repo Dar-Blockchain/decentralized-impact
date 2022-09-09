d4f2b31ca02df7ae150581fcf077bbd7452342fc
khaldi505 <ihebkhaldi505@gmail.com>
2022-09-09 11:11:05 +0100

    updating routes & localstorage info

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -53,10 +53,8 @@ const Login = () => {
       password: inputState.input.password,
     }).then((res) => {
       console.log(res.data.user._id)
-      localStorage.setItem('Token', res.data.token)
       localStorage.setItem('UserData', res.data.user)
       localStorage.setItem('loginStatus', 'logged')
-      localStorage.setItem('userType', 'Admin')
       // Admin Project confirmation, user can access confirmed projects 
         // window.location.reload()
     }).catch((err) => {
