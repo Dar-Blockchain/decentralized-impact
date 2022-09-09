570c05476a29049193282ffbf0158265a71a0280
khaldi505 <ihebkhaldi505@gmail.com>
2022-09-09 16:28:59 +0100

    create project*

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -7,7 +7,7 @@ const CreateProject = () => {
         category: "blockchain",
         firstTeamMemberEmail: ["hello"],
         description: "",
-        state: ""
+        state: "test"
     })
 
 
@@ -74,7 +74,7 @@ const CreateProject = () => {
         }).catch((err) => {console.error(err)})
     }
     return (
-    <form>
+    <>
 
         <h1>Create Project</h1>
             <div class="mb-6">
@@ -106,8 +106,8 @@ const CreateProject = () => {
             <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"></label>
             <textarea onChange={handleDescriptionInputChange} id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Project description"></textarea>
 
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={submit}>Submit Project</button>
-    </form>
+            <button onClick={submit} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit Project</button>
+    </>
       )
 }
 
