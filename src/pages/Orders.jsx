f1b907a9ba08a9c76efc03c34171273181a227f7
khaldi505 <ihebkhaldi505@gmail.com>
2022-09-13 13:43:58 +0100

    cleaning the new branch for the new template"

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+const Orders = () => {
+  const [loading, setLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+  
+  const getProjects = async () => {
+    const resp = await axios.get('https://decentralized-impact.alwaysdata.net/project/getprojects')  
+    setProjects(resp.data.project)
+    console.log(resp)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (loading === true){
+      getProjects()
+    }
+    
+}, [projects])
+
+  
+  return (
+
+    <div className="container text-center">
+    <div className="row">
+    {
+      loading ? ('error'):projects.map((project, index) => {
+       return(
+        <div className="col-2" key={index}>
+        <div className="card" >
+        <div className="card-body">
+          <h5 className="card-title">{project.title}</h5>
+          <p className="card-text">{project.description}</p>
+          <a href="#" className="btn btn-primary">{project.isConfirmed}</a>
+        </div>
+      </div>
+        </div>
+
+       )
+      }) 
+    }
+      
+    
+    
+      </div>
+    </div>
+  
+  )
+}
+
+export default Orders
