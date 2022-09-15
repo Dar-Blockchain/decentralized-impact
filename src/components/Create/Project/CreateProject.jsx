2fc477d20f82383342d22e4227fbfd1eabe10213
khaldi505 <ihebkhaldi505@gmail.com>
2022-09-15 18:48:07 +0100

    updates

--- src/components/Create/Project/CreateProject.jsx
+++ src/components/Create/Project/CreateProject.jsx
@@ -1,14 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Flex, Button, FormControl,Input, FormHelperText,FormLabel,Box ,Select, option, CheckboxGroup } from '@chakra-ui/react'
 import axios from 'axios'
 
-
 const CreateProject = () => {
+  const [teamMembers, setTeamMembers] = useState([''])
+
+  const addHandler = () => {
+    setTeamMembers([...teamMembers, ''])
+  }
+
+  const setTeamMember = (e) => {
+      const newArr = teamMembers
+      newArr[e.target.id] = e.target.value
+      console.log(newArr)
+      setTeamMembers(newArr)
+}
+
+  const titleHandler =  
+  const [project, setProject] = useState(
+    {
+      title: "",
+      category: "",
+      teamMemberEmails: teamMembers,
+      description: "",
+      descriptionFileUrl:"",
+      state: ""
+  }
+  )
+  const postProject = () => {
+    axios.post('https://decentralized-impact.alwaysdata.net/project/addproject', project).then((resp) => {
+      console.log(resp)
+    }).then((err) => {
+      console.log(err)
+    })
+  }
+
   return (
-    
-    <div>
-    
-    
-    </div>
+    <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
+    <Flex 
+    direction='column'
+    w={{ base: "100%", md: "420px" }}
+    maxW='100%'
+    background='transparent'
+    borderRadius='15px'
+    mx={{ base: "auto", lg: "unset" }}
+    me='auto'
+    mb={{ base: "20px", md: "auto" }}
+    alignSelf='center'
+    >
+    <FormControl id="projecTitle">
+      <FormLabel>Project title</FormLabel>
+      <Input type="text" />
+    </FormControl>
+    <FormControl id="teamMemberEmails">
+      <FormLabel>team memebers email</FormLabel>
+      <Flex className='teamMemberInSection' direction={'column'}  >
+        {teamMembers.map((value, index) => {
+          return(<Input onChange={setTeamMember} id={index}  type="email" />)
+        })}
+      </Flex>
+      <Button onClick={addHandler}>Add</Button>
+      <FormHelperText>add one more memeber by pressing the add button</FormHelperText>
+    </FormControl>
+    <FormControl id="description">
+      <FormLabel>Project Description</FormLabel>
+      <Input type="text" />
+      <FormHelperText>Project description</FormHelperText>
+    </FormControl>
+    <Select placeholder='Select project category'>
+      <option value='FOOD & HEALTHCARE'>FOOD & HEALTHCARE</option>
+      <option value='ENERGY & ENVIRONMENT'>ENERGY & ENVIRONMENT</option>
+      <option value='LOGISTICS & MOBILITY'>LOGISTICS & MOBILITY</option>
+      <option value='FINANCIAL INCLUSION'>FINANCIAL INCLUSION</option>
+      <option value='CREATIVE INDUSTRIES'>CREATIVE INDUSTRIES</option>
+      <option value='SOCIAL & CIVIC TECH'>SOCIAL & CIVIC TECH</option>
+  </Select>
+    </Flex>
+    </Box>
   )
 }
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,31 +3,78 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { Spinner } from '@chakra-ui/react'
-import ColumnsTable from 'views/admin/dataTables/components/ColumnsTable'
+import ComplexTable from "views/admin/default/components/ComplexTable";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+  } from "@chakra-ui/react"
+
+
+
+
 const Projects = () => {
+
+
+
+    const columnsDataComplex = [
+        {
+          Header: "NAME",
+          accessor: "name",
+        },
+        {
+          Header: "STATUS",
+          accessor: "status",
+        },
+        {
+          Header: "DATE",
+          accessor: "date",
+        },
+        {
+          Header: "PROGRESS",
+          accessor: "progress",
+        },
+      ];
+    
+    const [tableData, setTableData] = useState([])
     const [projects, setProjects] = useState([])
     const [loader, setLoader] = useState(true);
     const [error, setError] = useState('')
 
     const getProjects = async () => {
         const resp = await axios.get('https://decentralized-impact.alwaysdata.net/project/getprojects')  
-        console.log(resp)
         setProjects(resp.data.project)
+        console.log(resp)
+        console.log('hello')
         setLoader(false)
-    }
+      }
 
 
-    useEffect(() => {
-        if (loader === true){
-          getProjects()
-        }
-        
-    }, [projects])
+        useEffect(() => {
+            if (loader === true){
+            getProjects()
+            }
+            
+        }, [projects])
 
 
-
-
-
+    // const tableDataComplex = [
+    //     {
+    //       "name":"Horizon UI PRO",
+    //       "status": "Approved",
+    //       "date": "18 Apr 2022",
+    //       "progress": 75.5  
+    //     }
+    //   ]
+//     <ComplexTable
+//     columnsData={columnsDataComplex}
+//     tableData={tableDataComplex}
+//   />
 
 
 
@@ -40,11 +87,29 @@ return (
             loader === true ? (
                 <Spinner />
             ):(
-                projects.map((value, index) => {
-                    
-                }) 
-            )
-        }
+            <Table variant="simple" mt={'84px'}>
+                <TableCaption>Projects</TableCaption>
+                <Thead>
+                    <Tr>
+                    <Th>Title</Th>
+                    <Th>category</Th>
+                    <Th isNumeric>state</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                {
+                    projects.map((value, index) => {
+                        console.log(value)
+                    return (<Tr key={index}>
+                    <Td>{value.title}</Td>
+                    <Td>{value.category}</Td>
+                    <Td>{value.state}</Td>
+                    </Tr>)
+                })}
+                </Tbody>
+                </Table>
+                )
+            }        
     </div>
   )
 }
